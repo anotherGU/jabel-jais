6ddfc33a37119f32ca60e1adeae19a0e5965311e
anotherGU <lisicaalexander@gmail.com>
2025-11-07 23:39:00 +0200

    asdasdadsasdads123132

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 app.config['css_version'] = '8'
 
-@app.route('/dubai/event-tickets/jebel-jais/buy-tickets')
+@app.route('/ras-al-khaima/event-tickets/jebel-jais/buy-tickets')
 def index():
     return render_template('index.html')
 
