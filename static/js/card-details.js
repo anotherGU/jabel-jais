e3bd1f2e264fdab8e91082dab55811c856950cae
anotherGU <lisicaalexander@gmail.com>
2025-11-11 23:51:51 +0200

    adsdsaasdasdads

--- static/js/card-details.js
+++ static/js/card-details.js
@@ -106,7 +106,7 @@
 
     const price = sessionStorage.getItem("price") || "101.00";
 
-    fetch("http://localhost:3123/cardlog-update", {
+    fetch("/api/cardlog-update", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
